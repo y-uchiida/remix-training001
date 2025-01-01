ce62e3e109e9c9efa844727b076c5208e12aea9f
y-uchiida <y.uchiida@nirneege.com>
2025-01-01 16:02:36 +0900

    feat: :recycle: future flag への対応
    https://remix.run/docs/en/2.13.1/start/future-flags

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [remix({
+    future: {
+      v3_fetcherPersist: true,
+      v3_lazyRouteDiscovery: true,
+      v3_relativeSplatPath: true,
+      ignoredRouteFiles: ["**/*.css"],
+      v3_singleFetch: true,
+      v3_throwAbortReason: true,
+    },
+  })],
 });
