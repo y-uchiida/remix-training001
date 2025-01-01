d68efe0d818a4ffc1d3718de0576e517f3e821d8
y-uchiida <y.uchiida@nirneege.com>
2025-01-01 15:45:39 +0900

    feat: :technologist: 開発環境用と本番環境用で起動スクリプトを分ける
    - `npm run dev`: 開発モードとしてVite を起動する
    - `npm run start`: 本番モードとしてビルドされたアプリケーションを起動する

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 import { createRequestHandler } from "@remix-run/express";
 import express from "express";
- 
-// `remix vite:build` の結果は「単なるモジュール」であることに注意してください
-import * as build from "./build/server/index.js";
- 
+
+// 環境変数から実行モード(develop/production)を取得し、
+// 開発モードの場合はViteの開発サーバーを起動
+const viteDevServer =
+  process.env.NODE_ENV === "production"
+    ? null
+    : await import("vite").then((vite) =>
+        vite.createServer({
+          server: { middlewareMode: true },
+        })
+      );
+
 const app = express();
-app.use(express.static("build/client"));
- 
+
+// 開発モードの場合はViteの開発サーバーをミドルウェアとして登録
+// 本番モードの場合はビルド済みのクライアントアプリを提供
+app.use(
+	viteDevServer
+		? viteDevServer.middlewares
+		: express.static("build/client")
+);
+
+// サーバーサイドのビルドを行う関数を取得
+const build = viteDevServer
+  ? () =>
+      viteDevServer.ssrLoadModule(
+        "virtual:remix/server-build"
+      )
+  : await import("./build/server/index.js");
+
 // そして、アプリは「単なるリクエストハンドラー」です
 app.all("*", createRequestHandler({ build }));
- 
+
 app.listen(3000, () => {
   console.log("App listening on http://localhost:3000");
 });
